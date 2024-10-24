f8b59817fb0aaa57ea871855e97ec729f2db9421
Брюханов Сергей <sv.bruhanov@tensor.ru>
2024-10-24 08:42:38 +0300

    update

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-import aiosqlite
 import asyncio
 import logging
+from random import randint
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import F
 
 from quiz_data import quiz_data
-
+from db_methods import *
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = '7643506804:AAE4G9HLHbL65Y2ZVPKevYTDBadx0u9szkg'
-
-# Объект бота
 bot = Bot(token=API_TOKEN)
-# Диспетчер
 dp = Dispatcher()
-
-# Зададим имя базы данных
-DB_NAME = 'quiz_bot.db'
 
 
 def generate_options_keyboard(answer_options, correct_answer):
@@ -28,21 +23,32 @@ def generate_options_keyboard(answer_options, correct_answer):
     for option in answer_options:
         builder.add(types.InlineKeyboardButton(
             text=option,
-            callback_data="correct_answer" if option == correct_answer else "wrong_answer")
+            callback_data="right_answer" if option == correct_answer else "wrong_answer")
         )
 
     builder.adjust(1)
     return builder.as_markup()
 
 
-@dp.callback_query(F.data == "correct_answer")
-async def correct_answer(callback: types.CallbackQuery):
-
+async def current_question_update(current_question_index, callback):
+    # Обновление номера текущего вопроса в базе данных
     await callback.bot.edit_message_reply_markup(
         chat_id=callback.from_user.id,
         message_id=callback.message.message_id,
         reply_markup=None
     )
+
+    current_question_index += 1
+    await update_quiz_index(callback.from_user.id, current_question_index)
+
+    if current_question_index < len(quiz_data):
+        await get_question(callback.message, callback.from_user.id)
+    else:
+        await callback.message.answer("Это был последний вопрос. Квиз завершен!")
+
+
+@dp.callback_query(F.data == "right_answer")
+async def right_answer(callback: types.CallbackQuery):
 
     await callback.message.answer("Верно!")
     current_question_index = await get_quiz_index(callback.from_user.id)
@@ -50,38 +56,17 @@ async def correct_answer(callback: types.CallbackQuery):
     current_question_index += 1
     await update_quiz_index(callback.from_user.id, current_question_index)
 
-    if current_question_index < len(quiz_data):
-        await get_question(callback.message, callback.from_user.id)
-    else:
-        await callback.message.answer("Это был последний вопрос. Квиз завершен!")
-
 
 @dp.callback_query(F.data == "wrong_answer")
 async def wrong_answer(callback: types.CallbackQuery):
-    await callback.bot.edit_message_reply_markup(
-        chat_id=callback.from_user.id,
-        message_id=callback.message.message_id,
-        reply_markup=None
-    )
 
     # Получение текущего вопроса из словаря состояний пользователя
     current_question_index = await get_quiz_index(callback.from_user.id)
     correct_option = quiz_data[current_question_index]['correct_option']
 
-    await callback.message.answer(f"Неправильно. Правильный ответ: "
-                                  f"{quiz_data[current_question_index]['options'][correct_option]}")
-
-    # Обновление номера текущего вопроса в базе данных
-    current_question_index += 1
-    await update_quiz_index(callback.from_user.id, current_question_index)
-
-    if current_question_index < len(quiz_data):
-        await get_question(callback.message, callback.from_user.id)
-    else:
-        await callback.message.answer("Это был последний вопрос. Квиз завершен!")
+    await callback.message.answer(f"Неправильно. Правильный ответ: {quiz_data[current_question_index]['options'][correct_option]}")
 
 
-# Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     builder = ReplyKeyboardBuilder()
@@ -106,43 +91,12 @@ async def new_quiz(message):
     await get_question(message, user_id)
 
 
-async def get_quiz_index(user_id):
-    async with aiosqlite.connect(DB_NAME) as db:
-        # Получаем запись для заданного пользователя
-        async with db.execute('SELECT question_index FROM quiz_state WHERE user_id = (?)', (user_id, )) as cursor:
-            # Возвращаем результат
-            results = await cursor.fetchone()
-            if results is not None:
-                return results[0]
-            else:
-                return 0
-
-
-async def update_quiz_index(user_id, index):
-    # Создаем соединение с базой данных (если она не существует, она будет создана)
-    async with aiosqlite.connect(DB_NAME) as db:
-        # Вставляем новую запись или заменяем ее, если с данным user_id уже существует
-        await db.execute('INSERT OR REPLACE INTO quiz_state (user_id, question_index) VALUES (?, ?)', (user_id, index))
-        # Сохраняем изменения
-        await db.commit()
-
-
-# Хэндлер на команду /quiz
 @dp.message(F.text == "Начать игру")
 @dp.message(Command("quiz"))
 async def cmd_quiz(message: types.Message):
 
     await message.answer(f"Давайте начнем квиз!")
     await new_quiz(message)
-
-
-async def create_table():
-    # Создаем соединение с базой данных (если она не существует, она будет создана)
-    async with aiosqlite.connect(DB_NAME) as db:
-        # Создаем таблицу
-        await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (user_id INTEGER PRIMARY KEY, question_index INTEGER)''')
-        # Сохраняем изменения
-        await db.commit()
 
 
 async def main():
