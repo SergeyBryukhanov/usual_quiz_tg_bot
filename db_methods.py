1cfb037f60d40436ee55597183bdc468bcaf5db2
Брюханов Сергей <sv.bruhanov@tensor.ru>
2024-10-29 21:43:52 +0300

    update

--- db_methods.py
+++ db_methods.py
@@ -16,19 +16,57 @@ async def get_quiz_index(user_id):
                 return 0
 
 
-async def update_quiz_index(user_id, index):
+async def update_quiz(user_id, index, user_score):
     # Создаем соединение с базой данных (если она не существует, она будет создана)
     async with aiosqlite.connect(DB_NAME) as db:
         # Вставляем новую запись или заменяем ее, если с данным user_id уже существует
-        await db.execute('INSERT OR REPLACE INTO quiz_state (user_id, question_index) VALUES (?, ?)', (user_id, index))
+        await db.execute('INSERT OR REPLACE INTO quiz_state (user_id, question_index, user_score) VALUES (?, ?, ?)',
+                         (user_id, index, user_score))
         # Сохраняем изменения
         await db.commit()
+
+
+async def get_user_score(user_id):
+    # Подключаемся к базе данных
+    async with aiosqlite.connect(DB_NAME) as db:
+        # Получаем запись для заданного пользователя
+        async with db.execute('SELECT user_score FROM quiz_state WHERE user_id = (?)', (user_id,)) as cursor:
+            # Возвращаем результат
+            results = await cursor.fetchone()
+            if results is not None:
+                return results[0]
+            else:
+                return 0
+
+
+# async def update_user_score(user_id, user_score):
+#     # Создаем соединение с базой данных (если она не существует, она будет создана)
+#     async with aiosqlite.connect(DB_NAME) as db:
+#         # Вставляем новую запись или заменяем ее, если с данным user_id уже существует
+#         await db.execute('INSERT OR REPLACE INTO quiz_state (user_id, user_score) VALUES (?, ?)',
+#                          (user_id, user_score))
+#         # Сохраняем изменения
+#         await db.commit()
+
+
+async def get_all_users_score():
+    # Подключаемся к базе данных
+    async with aiosqlite.connect(DB_NAME) as db:
+        # Получаем счёт всех пользователей
+        async with db.execute('SELECT * FROM quiz_state') as cursor:
+            # Возвращаем результат
+            rows = await cursor.fetchall()
+            if rows is not None:
+                return rows
+            else:
+                return 0
 
 
 async def create_table():
     # Создаем соединение с базой данных (если она не существует, она будет создана)
     async with aiosqlite.connect(DB_NAME) as db:
         # Создаем таблицу
-        await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (user_id INTEGER PRIMARY KEY, question_index INTEGER)''')
+        await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (user_id INTEGER PRIMARY KEY, 
+                         question_index INTEGER, user_score INTEGER)''')
         # Сохраняем изменения
         await db.commit()
--- main.py
+++ main.py
@@ -20,25 +20,26 @@ def generate_options_keyboard(answer_options, correct_answer):
     builder = InlineKeyboardBuilder()
 
     for option in answer_options:
+        is_correct = "1" if option == correct_answer else "0"
         builder.add(types.InlineKeyboardButton(
             text=option,
-            callback_data="right_answer" if option == correct_answer else "wrong_answer")
-        )
-
+            callback_data=f"{option}_{is_correct}"))
     builder.adjust(1)
     return builder.as_markup()
 
 
-async def current_question_update(current_question_index, callback):
+async def db_update(current_question_index, callback, correct=False):
     # Обновление номера текущего вопроса в базе данных
     await callback.bot.edit_message_reply_markup(
         chat_id=callback.from_user.id,
         message_id=callback.message.message_id,
         reply_markup=None
     )
-
+    user_score = await get_user_score(callback.from_user.id)
+    if correct:
+        user_score += 1
     current_question_index += 1
-    await update_quiz_index(callback.from_user.id, current_question_index)
+    await update_quiz(callback.from_user.id, current_question_index, user_score)
 
     if current_question_index < len(quiz_data):
         await get_question(callback.message, callback.from_user.id)
@@ -46,34 +47,44 @@ async def current_question_update(current_question_index, callback):
         await callback.message.answer("Это был последний вопрос. Квиз завершен!")
 
 
-@dp.callback_query(F.data == "right_answer")
+@dp.callback_query(lambda c: c.data.split("_")[1] == "1")
 async def right_answer(callback: types.CallbackQuery):
 
+    user_answer = callback.data.split("_")[0]
     current_question_index = await get_quiz_index(callback.from_user.id)
 
-    await callback.message.answer("Верно!")
-    await current_question_update(current_question_index, callback)
+    await callback.message.answer(f"{user_answer} - Верно!")
+    await db_update(current_question_index, callback, correct=True)
 
 
-@dp.callback_query(F.data == "wrong_answer")
+@dp.callback_query(lambda c: c.data.split("_")[1] == "0")
 async def wrong_answer(callback: types.CallbackQuery):
-
     current_question_index = await get_quiz_index(callback.from_user.id)
     correct_option = quiz_data[current_question_index]['correct_option']
-
-    await callback.message.answer(f"Неправильно. Правильный ответ: {quiz_data[current_question_index]['options'][correct_option]}")
-    await current_question_update(current_question_index, callback)
+    user_answer = callback.data.split("_")[0]
+    correct_answer = quiz_data[current_question_index]['options'][correct_option]
+    await callback.message.answer(f"Неправильно. Ваш ответ: {user_answer}. Правильный ответ: {correct_answer}")
+    await db_update(current_question_index, callback)
 
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     builder = ReplyKeyboardBuilder()
     builder.add(types.KeyboardButton(text="Начать игру"))
+    builder.add(types.KeyboardButton(text="Показать счет"))
     await message.answer("Добро пожаловать в квиз!", reply_markup=builder.as_markup(resize_keyboard=True))
 
 
-async def get_question(message, user_id):
+@dp.message(F.text == "Показать счет")
+@dp.message(Command("score"))
+async def cmd_score(message: types.Message):
+    await message.answer("Таблица результатов")
+    rows = await get_all_users_score()
+    for row in rows:
+        await message.answer("Участник {user_id} счет: {user_score}/10".format(user_id=row[0], user_score=row[2]))
 
+
+async def get_question(message, user_id):
     # Получение текущего вопроса из словаря состояний пользователя
     current_question_index = await get_quiz_index(user_id)
     correct_index = quiz_data[current_question_index]['correct_option']
@@ -85,14 +96,14 @@ async def get_question(message, user_id):
 async def new_quiz(message):
     user_id = message.from_user.id
     current_question_index = 0
-    await update_quiz_index(user_id, current_question_index)
+    user_score = 0
+    await update_quiz(user_id, current_question_index, user_score)
     await get_question(message, user_id)
 
 
 @dp.message(F.text == "Начать игру")
 @dp.message(Command("quiz"))
 async def cmd_quiz(message: types.Message):
-
     await message.answer(f"Давайте начнем квиз!")
     await new_quiz(message)
 
